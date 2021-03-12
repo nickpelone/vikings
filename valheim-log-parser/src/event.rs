240060c31f55c9d3ba908577f17807038a60921a
Nick Pelone <nick.pelone@calyptix.com>
2021-03-12 15:01:54 -0500

    Basic modeling of character spawns and world saves

--- valheim-log-parser/src/event.rs
+++ valheim-log-parser/src/event.rs
@@ -1,9 +1,27 @@
-pub struct EventData {}
+use chrono::NaiveDateTime;
 
+/// placeholder struct until I spec out more events and their relevant data
+#[derive(Debug, Clone)]
+pub struct EventData;
+
+#[derive(Debug, Clone)]
+pub struct SpawnData {
+    pub timestamp: NaiveDateTime,
+    pub character: String,
+    pub location: (i64, i64),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct SaveData {
+    pub timestamp: NaiveDateTime,
+    pub time_spent: f64
+}
+
+#[derive(Debug, Clone)]
 pub enum Event {
     UserConnected(EventData),
     UserDisconnected(EventData),
-    WorldSaved(EventData),
-    CharacterDied(EventData),
-    CharacterRespawned(EventData)
+    WorldSaved(SaveData),
+    CharacterDied(SpawnData),
+    CharacterSpawned(SpawnData)
 }
