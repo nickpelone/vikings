bb3ca6aa43c2e5bca1694718b9303260648ace79
Nick Pelone <nick.pelone@calyptix.com>
2021-03-15 11:36:15 -0400

    Track a basic example that shows how we could assemble state over lines

---
+++ valheim-log-parser/examples/build-active-state.rs
@@ -0,0 +1,52 @@
+use valheim_log_parser::parse;
+
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::collections::HashMap;
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("./ValheimServerLogs2Login.txt")?;
+    let reader = BufReader::new(f);
+
+    let events = reader.lines().filter_map(|x| {
+        if let Ok(s) = x {
+            parse(&s)
+        } else {
+            None
+        }
+    });
+
+    let mut state: HashMap<u64, String> = HashMap::new();
+    let mut pending_steam_ids: Vec<u64> = Vec::new();
+    let mut pending_characters: Vec<String> = Vec::new();
+
+    for e in events {
+        println!("Got event: {:?}", e);
+        match e {
+            valheim_log_parser::Event::UserConnected(cd) => {
+                pending_steam_ids.push(cd.steam_id);
+
+            }
+            valheim_log_parser::Event::CharacterSpawned(sd) => {
+                pending_characters.push(sd.character);
+            },
+            _ => {}
+        };
+
+        if let Some(id) = pending_steam_ids.get(0) {
+            if let Some(c) = pending_characters.get(0) {
+                state.insert(*id, c.clone());
+                pending_characters.remove(0);
+                pending_steam_ids.remove(0);
+            }
+        }
+
+        println!("The state is now:");
+        println!("{:#?}", state);
+    }
+
+    println!("Final state");
+    println!("{:#?}", state);
+
+    Ok(())
+}
